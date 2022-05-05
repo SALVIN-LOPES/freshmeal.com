11b017890092a9233ab7c00cd58feba2c6b78b55
Salvin Lopes <salvinlopes20@gmail.com>
2022-05-05 23:26:56 +0530

    uploading to git

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//     $('.food-slider').slick({
+//       slidesToShow:3,
+//     });
+//   });
+// ----------------------------------------------MAIN-WRAPPER JS-------------------------------------------------
+$(document).ready(function(){
+
+    $('.nav-trigger').click(function(){
+        $('.site-content-wrapper').toggleClass('scaled');
+    });
+
+});
